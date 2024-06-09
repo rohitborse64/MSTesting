8e33053cb398a3b6346e309a46c1836f12d740e9
Rohit Borse <72754145+rohitborse64@users.noreply.github.com>
2024-06-09 18:17:37 +0530

    remove all comments from grade calculatorFeature/story1 create merge conflict (#2)
    * adding num3 property in grade calculator
    
    * Revert "adding num3 property in grade calculator"
    
    This reverts commit f62f1d71e95aac9c5e0dc098bb4c6472503d551f.

--- MSUnitTests/Model/GradeCalculator.cs
+++ MSUnitTests/Model/GradeCalculator.cs
@@ -1,10 +1,4 @@
-// change1 adding on remote location.
-// change2 adding on locally
-// change3 on feature branch story1
-// change4 on remote
-// change5 to create conflict
-// adding more changes
-// change5 to create 
+
 namespace MSUnitTests.Model
 {
     public class GradeCalculator
