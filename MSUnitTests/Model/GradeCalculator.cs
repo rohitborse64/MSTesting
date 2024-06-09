f364dff5f8f95fb6085c40fa700a4c770aaf5b23
Rohit Borse <rohitborse64@gmail.com>
2024-06-09 17:28:41 +0530

    conflict

--- MSUnitTests/Model/GradeCalculator.cs
+++ MSUnitTests/Model/GradeCalculator.cs
@@ -2,13 +2,13 @@
 // change2 adding on locally
 // change3 on feature branch story1
 // change4 on remote
-// change5 to create conflict
-
+// change5 to create 
 namespace MSUnitTests.Model
 {
     public class GradeCalculator
     {
         public int num1 { get; set; }
         public int num2 { get; set; }
+
     }
 }
