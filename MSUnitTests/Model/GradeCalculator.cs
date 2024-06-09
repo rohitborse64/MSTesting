0e4dfd69ea8ec5972a1f16c29ef3e5077194bca1
Rohit Borse <rohitborse64@gmail.com>
2024-06-09 17:07:01 +0530

    adding gradecalculator property

--- MSUnitTests/Model/GradeCalculator.cs
+++ MSUnitTests/Model/GradeCalculator.cs
@@ -3,3 +3,12 @@
 // change3 on feature branch story1
 // change4 on remote
 // change5 to create conflict
+
+namespace MSUnitTests.Model
+{
+    public class GradeCalculator
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+    }
+}
